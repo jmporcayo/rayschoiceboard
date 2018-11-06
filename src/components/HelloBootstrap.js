9ca08a50f96a6bdf7a5c2f1c4c6f12966b7b41f0
JMP <porcayojm@gmail.com>
2018-11-05 20:37:05 -0800

    add on ResponsiveVoice.JS

--- src/components/HelloBootstrap.js
+++ src/components/HelloBootstrap.js
@@ -1,129 +1,78 @@
 import React from "react";
 import "./HelloBootstrap.css";
+import Lightbox from "react-lightbox-component";
 
   // build an array of url
   var imageUrl = [
-    "abby",
-    "babybear",
-    "bert",
-    "bigbird",
-    "cookie",
-    "elmo",
-    "ernie",
-    "grover",
-    "rosita",
-    "snuffy",
-    "telly",
-    "thecount",
+    "shower",
+    "potty",
+    "brushyourteeth",
+    "getdressed",
+    "book",
+    "juice",
+    "snack",
+    "thirsty",
+    "toys",
+    "sleep",
   ];
 
 class HelloBootstrap extends React.Component {
   constructor(props){
     super(props);
     
-    this.state = {
-      guesses : [],
-      current: 0,
-      high: 0
-    }
-    // guesses, current score, high score
-
-    this.cardClick = this.cardClick.bind(this);
-    this.shuffle = this.shuffle.bind(this);    
-  }
-
-   shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-  
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-  
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-  
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-  
-    return array;
+ 
+   
+   this.cardClick = this.cardClick.bind(this);
   }
 
   cardClick(e){
-    console.log(e.target.name)
+    console.log(e.target.name);
+    window.responsiveVoice.speak(e.target.name);
 
-    imageUrl = this.shuffle(imageUrl);
-
-    let guesses = this.state.guesses;
-    let current = this.state.current;
-    let high = this.state.high;
-    // value is in an array
-     if (guesses.indexOf(e.target.name)=== -1) {
-       guesses.push(e.target.name);
-       current++;
-       this.setState({
-         guesses,
-        current
-       })
-     }
-    else
-    {
-      if (current > high){
-        high = current;
-      }
-      this.setState({
-        guesses: [],
-        current: 0,
-        high
-      })
-    }
+    this.setState({
+      currentImage : e.target.name
+    })
   }
   
   render(){
+   
+
+
+
     return(
-  <div className="container">
+      <div>
+      <div className="container">
 
-     {/* Begin NavBar */}
-    <nav className="navbar">
-        <ul>
-          <li className="clickygame">
-          <a href="/">Clicky Game</a>
-          </li>
-          <li className="">Click an image to begin!</li>
-          <li>Score: {this.state.current} | Top Score: {this.state.high}</li>
-          </ul>
-    </nav>
-    {/* End NavBar */}
+        {/* Begin NavBar */}
+        <nav className="navbar">
+            <ul>
+              <li className="clickygame">
+              <a href="/">Ray's Choice Board</a>
+              </li>
+              
+              </ul>
+        </nav>
 
-    <div className="jumbotron">
-      <h1> The Sesame Street Game!</h1>
-      <p>
-      Click on an image to earn points, but don't click on any more than once!
-      </p>
-    </div>
-    {/* End jumbotron */}
+        {/* End NavBar */}
 
- {/* name of my react component */}
-  { imageUrl.map(x => <ImageRandom image={x} parentClick={this.cardClick} />) }   
-  </div>
+          <div className="jumbotron">
+          <h1> COMMUNICATE WITHOUT WORDS</h1>
+        </div> 
+
+        {/* End jumbotron */}
+
+    {/* name of my react component */}
+      { imageUrl.map(x => <ImageCard image={x} parentClick={this.cardClick} />) }   
+          
+      </div>
+      </div>
 
     )}
-  // array of already chosen images
-
-
-
-
-  //  logic if the user clicks on a card cross reference if its already in the chosen array if it is game over
-
-  // increment score
-
-  // if its a new img score goes up randomize images add to chosen img array
+ 
 
 };
 
-function ImageRandom(props){
+function ImageCard(props){
   return (
 
   <div className="card  mb-3" style={{'maxWidth': '10rem', 'maxHeight': '10rem'}}>
@@ -140,3 +89,12 @@ function ImageRandom(props){
 
 
 export default HelloBootstrap;
+
+
+// const App = () => (
+//   <div>
+
+//     <Lightbox images={imageUrl}  { src: "imageUrl",}/>
+
+//   </div>
+// );
