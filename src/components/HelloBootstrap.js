56bf3f3b69d40408bcd3512f490039afedae3d51
JMP <porcayojm@gmail.com>
2018-11-12 18:02:47 -0800

    Revert "Merge pull request #11 from jmporcayo/choiceboardclient"
    This reverts commit b3d9191c26696512f0d8775d8e35f81e72aff1c8, reversing
    changes made to d610a21c0c2ef98be255e85746b8851550425a43.

--- src/components/HelloBootstrap.js
+++ src/components/HelloBootstrap.js
@@ -79,11 +79,8 @@ class HelloBootstrap extends React.Component {
         {/* End jumbotron */}
 
     {/* name of my react component */}
-    <div className="row">
-    <div className="col-md-12">
       { imageUrl.map(x => <ImageCard image={x} parentClick={this.cardClick} />) }   
-        </div>  
-        </div>
+          
       </div>
       </div>
 
